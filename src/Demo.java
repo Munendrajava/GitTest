2bc1291dded3edd273d62f6486d19bf1ed7b0cd6
dell <munedra>
2021-12-10 21:37:58 +0530

    initial commit

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo
+{
+    public void display()
+    {
+        System.out.println("This is Display Method");
+    }
+    public static void main(String[] args)
+    {
+        Demo obj=new Demo();
+        obj.display();
+    }
+}
